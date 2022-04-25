cc7bf1a2a79592aa43f4450a270a24721e5fa6c2
Ateeq <ateeg1999@gmail.com>
2022-04-25 18:23:30 +0200

    edit config file

--- auth/server.js
+++ auth/server.js
@@ -17,6 +17,6 @@ app.post("/auth", function (req, res) {
   res.status(403).send();
 });
 
-app.listen(8000, function () {
-  console.log("Listening on port 8000!");
+app.listen(5000, function () {
+  console.log("Listening on port 5000!");
 });
